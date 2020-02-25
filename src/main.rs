06e31be48937e546e77f59f9640b44bdbe2e9671
Nearsyh <nearsy.h@gmail.com>
2020-02-25 19:10:12 +0800

    Bootstrap the server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer, Responder, Result};
+
+#[get("/health")]
+async fn health() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json(true))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
