816ab05db322efcbdae8ab4bf51a2cb44c83e545
Near <nearsy.h@gmail.com>
2020-02-29 19:04:07 +0800

    Add OPENSSL_DIR env

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
-# Remember to add target if you haven't
+## Remember to add target if you haven't
 # rustup target add rustup target add
 
-cargo build --target=x86_64-unknown-linux-gnu --release
+## Dependencies
+# brew install openssl
+
+OPENSSL_DIR=/usr/local/opt/openssl@1.1 cargo build --target=x86_64-unknown-linux-gnu --release
